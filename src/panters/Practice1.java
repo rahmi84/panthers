cd3c3821ebb069b7d14c3d986de24924a180064d
Fatih Kurt <mfkurt11@gmail.com>
2020-08-12 16:59:42 -0400

    Muhammet's Commit

--- src/panters/Practice1.java
+++ src/panters/Practice1.java
@@ -5,6 +5,8 @@ public class Practice1 {
 	public static void main(String[] args) {
 		System.out.println("first code from master repo");
 		System.out.println("first code from remote repo");
+		
+		System.out.println("Muhammet's first code from local repo");
 	}
 
 }
