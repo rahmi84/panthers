9e9651a79fd9f65d08dad8efda36f38ec78015ab
fcakgul <69127151+fcakgul@users.noreply.github.com>
2020-08-12 16:59:00 -0400

    Update Practice1.java
    first code from Fatma

--- src/practice/Practice1.java
+++ src/practice/Practice1.java
@@ -5,6 +5,9 @@ public class Practice1 {
 	public static void main(String[] args) {
 		System.out.println("first code from master");
 		System.out.println("first code from remote master");
+		
+		System.out.println("first code from Fatma");
+		
 
 	}
 
