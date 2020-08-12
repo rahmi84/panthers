c044d9dd56b81da53a73853091fb9a88bd0aab46
mfkurt11 <68709611+mfkurt11@users.noreply.github.com>
2020-08-12 17:04:51 -0400

    Update Practice1.java
    Muhammet's remote repo

--- src/practice/Practice1.java
+++ src/practice/Practice1.java
@@ -5,7 +5,8 @@ public class Practice1 {
 	public static void main(String[] args) {
 		System.out.println("first code from master");
 		System.out.println("first code from remote master");
-System.out.println("from deniz");
+		System.out.println("from deniz");
+		System.out.println("Muhammet's first code from remote repo");
 	}
 
 }
